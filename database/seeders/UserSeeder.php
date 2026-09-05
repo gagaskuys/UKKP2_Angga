66637218e32dbae83a41e9942aa536a6c8641898
gagaskuys <anggajayakusumah358@gmail.com>
2026-09-05 08:21:39 +0700

    update userseed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,25 +10,28 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // 1. Akun Admin
         User::create([
-            'name' => 'Super Admin',
-            'email' => 'admin@app.com',
-            'password' => Hash::make('password'),
-            'role' => 'admin',
+            'name'     => 'Super Admin',
+            'email'    => 'admin@gmail.com',
+            'password' => Hash::make('123456'),
+            'role'     => 'admin',
         ]);
 
+        // 2. Akun Petugas
         User::create([
-            'name' => 'Petugas Satu',
-            'email' => 'petugas@app.com',
-            'password' => Hash::make('password'),
-            'role' => 'petugas',
+            'name'     => 'Petugas Satu',
+            'email'    => 'petugas@gmail.com',
+            'password' => Hash::make('123456'),
+            'role'     => 'petugas',
         ]);
 
+        // 3. Akun Customer
         User::create([
-            'name' => 'Customer Demo',
-            'email' => 'customer@app.com',
-            'password' => Hash::make('password'),
-            'role' => 'customer',
+            'name'     => 'Customer Demo',
+            'email'    => 'customer@gmail.com',
+            'password' => Hash::make('123456'),
+            'role'     => 'customer',
         ]);
     }
 }
